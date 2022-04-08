79a80f2e1ea272ed39024144568459966893d1b2
Kawsar-8 <ahmednajma1998@gmail.com>
2022-04-08 14:18:29 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,37 @@
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int n1 = 48;
-	int n2, n3;
-	int com = 44;
-	while (n1 <= 57)
-	{
-		n2 = n1 + 1;
-		while (n2 <= 57)
-		{
-			n3 = n2 + 1;
-			while (n3 <= 57)
-			{
-				putchar(n1);
-				putchar(n2);
-				putchar(n3);
-				if (n1 != 55 || n2 != 56 || n3 != 57)
-				{
-					putchar(com);
-					putchar(32);
-				}
-				n3 += 1;
-			}
-			n2 += 1;
-		}
-		n1 += 1;
-	}
-	putchar('\n');
-	return (0);
-}
+1 #include <stdio.h>
+  2
+  3 /**
+  4  * main - Entry point
+  5  *
+  6  * Return: Always 0 (Success)
+  7  */
+  8
+  9 int main(void)
+ 10 {
+ 11         int n1 = 48;
+ 12         int a = 0;
+ 13         int b;
+ 14         int com = 44;
+ 15
+ 16         while (a <= 99)
+ 17         {
+ 18                 b = a + 1;
+ 19                 while (b <= 99)
+ 20                 {
+ 21                         putchar((a / 10) + n1);
+ 22                         putchar((a % 10) + n1);
+ 23                         putchar(32);
+ 24                         putchar((b / 10) + n1);
+ 25                         putchar((b % 10) + n1);
+ 26                         if (a != 98 || b != 99)
+ 27                         {
+ 28                                 putchar(com);
+ 29                                 putchar(32);
+ 30                         }
+ 31                         b += 1;
+ 32                 }
+ 33                 a += 1;
+ 34         }
+ 35         putchar('\n');
+ 36         return (0);
+ 37 }

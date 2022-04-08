5d26b31fe98d948b7f28588fa8acd96fd592dd3a
Kawsar-8 <ahmednajma1998@gmail.com>
2022-04-08 13:19:07 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 48;
+int l = 'a';
+while (n <= 57)
+{
+putchar(n);
+n += 1;
+}
+while (l <= 'f')
+{
+putchar(l);
+l++;
+}
+putchar('\n');
+return (0);
+}

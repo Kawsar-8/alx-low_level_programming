1414962920c1095f1430de6286c133699690672c
Kawsar-8 <ahmednajma1998@gmail.com>
2022-04-08 14:29:10 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,37 +1,37 @@
-1 #include <stdio.h>
-  2
-  3 /**
-  4  * main - Entry point
-  5  *
-  6  * Return: Always 0 (Success)
-  7  */
-  8
-  9 int main(void)
- 10 {
- 11         int n1 = 48;
- 12         int a = 0;
- 13         int b;
- 14         int com = 44;
- 15
- 16         while (a <= 99)
- 17         {
- 18                 b = a + 1;
- 19                 while (b <= 99)
- 20                 {
- 21                         putchar((a / 10) + n1);
- 22                         putchar((a % 10) + n1);
- 23                         putchar(32);
- 24                         putchar((b / 10) + n1);
- 25                         putchar((b % 10) + n1);
- 26                         if (a != 98 || b != 99)
- 27                         {
- 28                                 putchar(com);
- 29                                 putchar(32);
- 30                         }
- 31                         b += 1;
- 32                 }
- 33                 a += 1;
- 34         }
- 35         putchar('\n');
- 36         return (0);
- 37 }
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int n1 = 48;
+	int a = 0;
+	int b;
+	int com = 44;
+	while (a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			putchar((a / 10) + n1);
+			putchar((a % 10) + n1);
+			putchar(32);
+			putchar((b / 10) + n1);
+			putchar((b % 10) + n1);
+			if (a != 98 || b != 99)
+			{
+				putchar(com);
+				putchar(32);
+			}
+			b += 1;
+		}
+		a += 1;
+	}
+	putchar('\n');
+	return (0);
+}

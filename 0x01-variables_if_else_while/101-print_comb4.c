ef00f76124e4258fb780182ba0e17bce84e226ea
Kawsar-8 <ahmednajma1998@gmail.com>
2022-04-09 12:03:47 -0700

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n1 = 48;
 	int n2, n3;
 	int com = 44;
+
 	while (n1 <= 57)
 	{
 		n2 = n1 + 1;
